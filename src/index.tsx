913aefa9d283e5cca1570cd178dda4f325f5053b
熊猫 <xiongmao@xiongmaodeMacBook-Pro.local>
2023-06-07 14:55:32 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Scrypt } from 'scrypt-ts'
+
+import { Voting } from './contracts/voting';
+var artifact = require('../artifacts/src/contracts/voting.json');
+Voting.loadArtifact(artifact);
+
+Scrypt.init({
+  // https://docs.scrypt.io/advanced/how-to-integrate-scrypt-service#get-your-api-key
+  apiKey: 'testnet_1PitQQtL3GyZLdFpBaDC1LEC3DopNoaVCS6dDGX1tcvb23Fb2',
+  network: 'testnet'
+})
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
